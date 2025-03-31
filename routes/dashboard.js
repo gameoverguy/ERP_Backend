69b0fdddef9d56a15650b2b0e6de0b518ddc19e0
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2025-03-31 17:15:53 +0530

    31_03

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dashboardController = require("../controllers/dashboard.controller");
+const verifyToken = require("../middleware");
 const router = express.Router();
 
 router.get("/", dashboardController.index);
